0190210b2dea02b200c7554647f1b2778389ab0f
n0dam1 <noda_501_mi@hotmail.com>
2017-01-09 14:13:57 +0000

    add allow_blank

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,4 +1,4 @@
 class Tweet < ActiveRecord::Base
   validates :content, presence: true
-  validates :content, length: { in: 1..255}
+  validates :content, length: { in: 1..255 }, allow_blank: true
 end
